659252879a7ba99d8d676a83a2fbda80c78402c6
unknown <bmarshall074@wit-xdvm-049.MILLARDPS.NET>
2013-03-01 13:01:05 -0600

    Website finished

--- actions/edit_city.php
+++ actions/edit_city.php
@@ -1,5 +1,17 @@
 <?php
 session_start();
+?>
+<pre><?php //print_r($_POST);?></pre>
+
+<?php
+
+// Validate that each piece of info was provided
+if( $_POST['city_name'] != '' &&
+	$_POST['city_state'] != '' &&
+	$_POST['city_rank'] != '' &&
+	$_POST['city_pop'] != '' &&
+	$_POST['city_area'] != '') {
+	
 // Read file into array
 $lines = file('../data/cities.csv', FILE_IGNORE_NEW_LINES);
 
@@ -20,4 +32,18 @@ $_SESSION['message'] = array(
 );
 
 header('Location:../?p=list_cities');
+} else {
+	// Store submitted data into session data
+	$_SESSION['POST'] = $_POST;
+	
+	// Store error message in session data
+	$_SESSION['message'] = array(
+			'text' => 'Please enter all required information.',
+			'type' => 'error'
+	);
+	
+	// Redirect to the form
+	header("Location:../?p=form_edit_city&city={$_POST['linenum']}");
+}
+
 ?>
